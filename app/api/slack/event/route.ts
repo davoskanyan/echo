0c77d2f0e452c9654a82ba8f9a223d973f23a485
Davit Voskanyan <davitv@adobe.com>
2025-01-06 12:00:52 +0400

    fix: console error

--- app/api/slack/event/route.ts
+++ app/api/slack/event/route.ts
@@ -5,33 +5,41 @@ const channelId = process.env.SLACK_CHANNEL as string;
 let lastEventTime: number | undefined;
 
 export async function POST(request: Request) {
-  const { type, challenge, event } = await request.json();
+  try {
+    const { type, challenge, event } = await request.json();
 
-  if (type === 'url_verification') {
-    return Response.json({ challenge });
-  }
-
-  const eventTime = Number(event.ts);
-  if (lastEventTime && lastEventTime >= eventTime) {
-    return Response.json({});
-  }
-  lastEventTime = eventTime;
-
-  if (type === 'event_callback') {
-    const { type: eventType, text, channel, bot_id } = event;
-
-    if (eventType === 'message' && channel === channelId && !bot_id) {
-      const response = await fetch(`${baseUrl}/api/openapi`, {
-        method: 'POST',
-        body: JSON.stringify({ text }),
-      });
-      const answer = await response.json();
-      await fetch(`${baseUrl}/api/slack/message`, {
-        method: 'POST',
-        body: JSON.stringify({ text: answer.completion }),
-      });
+    if (type === 'url_verification') {
+      return Response.json({ challenge });
     }
-  }
 
-  return Response.json({});
+    const eventTime = Number(event.ts);
+    if (lastEventTime && lastEventTime >= eventTime) {
+      return Response.json({});
+    }
+    lastEventTime = eventTime;
+
+    if (type === 'event_callback') {
+      const { type: eventType, text, channel, bot_id } = event;
+
+      if (eventType === 'message' && channel === channelId && !bot_id) {
+        const response = await fetch(`${baseUrl}/api/openapi`, {
+          method: 'POST',
+          body: JSON.stringify({ text }),
+        });
+        const answer = await response.json();
+        await fetch(`${baseUrl}/api/slack/message`, {
+          method: 'POST',
+          body: JSON.stringify({ text: answer.completion }),
+        });
+      }
+    }
+
+    return Response.json({});
+  } catch (error: unknown) {
+    console.error(
+      'Error processing Slack event:',
+      JSON.stringify(error, null, 2),
+    );
+    return Response.json(error, { status: 500 });
+  }
 }
